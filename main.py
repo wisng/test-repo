4d1cb408d98dafc42ae1c48b9830849c1f3af457
Wisdom <wisdomkangngo@gmail.com>
2025-05-30 22:19:56 -0400

    removed sockets

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from flask_socketio import SocketIO
-from src.sockets.socket_relay_handler import SocketRelayHandler
 from src.routes.rest_routes import api
 from src.config.settings import settings
 
@@ -9,21 +7,13 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(api)
 
-    socketio = SocketIO()
-
-    socketio.init_app(app, cors_allowed_origins="*")
-
-    relay_handler = SocketRelayHandler(socketio)
-    relay_handler.initialize_handlers()
-
-    print("SocketIO initialized")
-    return app, socketio
+    print("Starting server...")
+    return app
 
 
-app, socketio = create_app()
+app = create_app()
 if __name__ == '__main__':
-    socketio.run(
-        app,
+    app.run(
         host=settings.flask.host,
         port=settings.flask.port
     )
